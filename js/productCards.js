771db6b402ca0e9b27a9578116012fa4b748fd5a
pattersonlabs <charlie.james.e.p@gmail.com>
2026-04-06 21:04:24 +0100

    Enhance product card styling and functionality by updating CSS for better layout, adding quantity controls, and integrating icons for add and subtract buttons.

--- js/productCards.js
+++ js/productCards.js
@@ -7,6 +7,7 @@ export function productCard(product) {
   element.classList.add("productCards");
 
   let image = document.createElement("img");
+  image.classList.add("product-image");
   image.setAttribute("src", product.image.thumbnail);
 
   let productCategory = document.createElement("h3");
@@ -23,12 +24,22 @@ export function productCard(product) {
 
   let addProductButton = document.createElement("button");
   addProductButton.classList.add("add-to-cart-btn");
-  addProductButton.innerText = "add to cart";
+
+  const addItemsToCartImg = document.createElement("img");
+  addItemsToCartImg.src = "assets/images/icon-add-to-cart.svg";
+  addItemsToCartImg.alt = "cart";
+  addProductButton.append(addItemsToCartImg, "Add to Cart");
 
   const div = document.createElement("div");
+  div.classList.add("quantity-controls");
+
   const addItemsBtn = document.createElement("button");
   addItemsBtn.classList.add("add-items-btn");
-  addItemsBtn.innerText = "+";
+
+  const plusImage = document.createElement("img");
+  plusImage.src = "assets/images/icon-increment-quantity.svg";
+  plusImage.alt = "add";
+  addItemsBtn.append(plusImage);
 
   const quantityNumber = document.createElement("span");
   quantityNumber.classList.add("quantity-number");
@@ -36,9 +47,12 @@ export function productCard(product) {
 
   const subtractItemsBtn = document.createElement("button");
   subtractItemsBtn.classList.add("subtract-items-btn");
-  subtractItemsBtn.innerText = "-";
+  const minusImage = document.createElement("img");
+  minusImage.src = "assets/images/icon-decrement-quantity.svg";
+  minusImage.alt = "mins";
+  subtractItemsBtn.append(minusImage);
 
-  div.append(addItemsBtn, quantityNumber, subtractItemsBtn);
+  div.append(subtractItemsBtn, quantityNumber, addItemsBtn);
 
   let currentQuantity = 0;
 
